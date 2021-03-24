b2a6d695b53929cc6bf0161e37877e3178578108
Noe Gautier <ngautier006@ensc.fr>
2021-03-24 09:56:22 +0100

    Noe commit

--- annuaireeleve/Connexion2.php
+++ annuaireeleve/Connexion2.php
@@ -1,31 +1,33 @@
 <?php
-//require_once "includes/functions.php";
+require_once "includes/functions.php";
 session_start();
 
 if (!empty($_POST['login']) and !empty($_POST['mdp']))
 {
     $login = $_POST['login'];
-    $password = $_POST['mdp'];
+    $mdp = $_POST['mdp'];
 
     if($_SESSION['user']=="eleve")
     {
-        $stmt = getDb()->prepare('select * from eleve where usr_login=? and usr_mdp=?');
+        $stmt = getDb()->prepare('select * from eleve where login=? and motDePasse=?');
         $stmt->execute(array($login, $mdp));
     }
     else
     {
-        $stmt = getDb()->prepare('select * from gestionnaire where usr_login=? and usr_mdp=?');
+        $stmt = getDb()->prepare('select * from gestionnaire where login=? and motDePasse=?');
         $stmt->execute(array($login, $mdp));
     }
     if ($stmt->rowCount() == 1) {
         // Authentication successful
         $_SESSION['login'] = $login;
-        redirect("acceuil.php");
+        redirect('Accueil.php');
+        echo "YAH";
     }
     else 
     {
+        echo"Bonjour";
         $_SESSION['message']=1;
-        header('Location: Connexion1.php');
+        redirect('Connexion.php');
     }
 }
 ?>
